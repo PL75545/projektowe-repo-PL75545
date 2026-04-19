3c0169d06d700abbe77cc27243cae26fe40e6ddf
Mykola Vynnichyk <mvynnic1@stu.vistula.edu.pl>
2026-04-19 21:00:24 +0200

    Update script.js

--- zad4/script.js
+++ zad4/script.js
@@ -19,3 +19,40 @@ function toggleProjekty() {
         section.style.display = "none";
     }
 }
+
+
+//  WALIDACJA FORMULARZA 
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value.trim();
+    let surname = document.getElementById("surname").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let message = document.getElementById("message").value.trim();
+    let error = document.getElementById("error");
+
+    error.style.color = "red";
+    error.textContent = "";
+
+    // puste pola
+    if (!name || !surname || !email || !message) {
+        error.textContent = "Wszystkie pola są wymagane!";
+        return;
+    }
+
+    // cyfry w imieniu/nazwisku
+    if (/\d/.test(name) || /\d/.test(surname)) {
+        error.textContent = "Imię i nazwisko nie mogą zawierać cyfr!";
+        return;
+    }
+
+    // email
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        error.textContent = "Niepoprawny email!";
+        return;
+    }
+
+    error.style.color = "green";
+    error.textContent = "Formularz wysłany poprawnie!";
+});
